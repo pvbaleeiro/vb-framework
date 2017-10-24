efa7c3aa3a45f82b2460884d3f01b91390e4edb5
Victor Baleeiro <victor.baleeiro@going2.com.br>
2017-10-24 12:38:20 -0200

    [dev] Commit inicial

---
+++ VBFramework/VBFramework/TouchIDManager.swift
@@ -0,0 +1,9 @@
+//
+//  TouchIDManager.swift
+//  VBFramework
+//
+//  Created by Victor Baleeiro on 24/10/17.
+//  Copyright © 2017 Going2. All rights reserved.
+//
+
+import Foundation
---
+++ VBFramework/VBFramework/VBFramework.h
@@ -0,0 +1,19 @@
+//
+//  VBFramework.h
+//  VBFramework
+//
+//  Created by Victor Baleeiro on 24/10/17.
+//  Copyright © 2017 Going2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VBFramework.
+FOUNDATION_EXPORT double VBFrameworkVersionNumber;
+
+//! Project version string for VBFramework.
+FOUNDATION_EXPORT const unsigned char VBFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VBFramework/PublicHeader.h>
+
+
